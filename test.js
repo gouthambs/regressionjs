56c0a6c33a385f49d2cbf88ddfcff7823f365e72
Gouthaman Balaraman <gouthambs@gmail.com>
2013-09-28 16:30:40 -0700

    Added test for mean and variances.

--- test.js
+++ test.js
@@ -25,7 +25,6 @@ exports.ols_estimator_test = function(test)
 	         [1.,-.25 ,-.40],
 	         [1.,.15 ,.65]];
 	var b = regression.olsEstimation(y,A);
-	//test.equal(b,[[-0.009283302118409553],[0.6660860367398023],[0.4485050801075223]]);
 	test.equal(b[0][0],-0.009283302118409553);
 	test.equal(b[1][0],0.6660860367398023);
 	test.equal(b[2][0],0.4485050801075223);
@@ -56,10 +55,32 @@ exports.wls_estimator_test = function(test)
 	         [1.,.15 ,.65]];
 	var b = regression.wlsEstimation(y,A);
 	
-	//test.equal(b,[[-0.009283302118409553],[0.6660860367398023],[0.4485050801075223]]);
 	test.equal(b[0][0],-0.003968253968253066);
 	test.equal(b[1][0],2.492063492063469);
 	test.equal(b[2][0], -0.23809523809523547);
+	test.done();
+};
+exports.mean_test = function(test)
+{
+	var V = [0,2,4,6,7];
+	var m = regression.mean(V);
+	test.equal(m,3.8,"Failed mean calculation test");
+	test.done();
+};
+exports.population_variance_test = function(test)
+{
+	var V = [0,2,4,6,7];
+	var m = regression.mean(V);
+	var pv = regression.populationVariance(V,m);
+	test.equal(pv,6.56,"Failed population variance test");
+	test.done();
+};
+exports.sample_variance_test = function(test)
+{
+	var V = [0,2,4,6,7];
+	var m = regression.mean(V);
+	var pv = regression.sampleVariance(V,m);
+	test.equal(pv,8.2,"Failed sample variance test");
 	test.done();
 };
 
